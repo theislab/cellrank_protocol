252c5b32b228012044c59fbf3b8e3a15f4efe471
Philipp Weiler <weiler.philipp@gmail.com>
2025-04-03 10:01:07 -0400

    Update constants
    Add constants `S_GENES` and `G2M_GENES`, defining marker genes cell cycle stages.

--- src/crp/core/__init__.py
+++ src/crp/core/__init__.py
@@ -1,3 +1,3 @@
-from ._constants import DATA_DIR, FIG_DIR
+from ._constants import DATA_DIR, FIG_DIR, G2M_GENES, S_GENES
 
-__all__ = ["DATA_DIR", "FIG_DIR"]
+__all__ = ["DATA_DIR", "FIG_DIR", "G2M_GENES", "S_GENES"]
